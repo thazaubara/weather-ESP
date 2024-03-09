42f625375f311d626cf156e809794c16cbc8fb02
Michael Hedl <hedl.worx@gmail.com>
2024-03-09 16:12:11 +0100

    more generic code
    easily configurable

--- weather-ESP.ino
+++ weather-ESP.ino
@@ -4,71 +4,66 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme;
-
-const char* SSID = "";
-const char* PSK = "";
-const char* MQTT_BROKER = "";
+// WIFI CREDENTIALS
+const char* SSID = "YOUR_SSID";
+const char* PSK = "YOUR_PASSWORD";
+const char* MQTT_BROKER = "MQTT_BROKER_IP";
 const int PORT = 1883;
 
-const float TEMP_SENS_OFFSET = -2;
-
+// LOOP DELAY
 const int DELAY = 2500;
 
+// MQTT STUFF
+String mqtt_subtopic = "wohnzimmer";
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+String espClientNameString = "ESPClient_" + mqtt_subtopic;
+char espClientName[50];
+
+// BME STUFF
+Adafruit_BME280 bme;
+const float TEMP_SENS_OFFSET = -2;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  // INIT LED. TURN IT ON
+  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+
+  // INIT SERIAL
 	Serial.begin(115200);
-	setup_wifi();
-	client.setServer(MQTT_BROKER, PORT);
-  connect_bme();
-}
+  Serial.println("\nWelcome to Weater-ESP");
 
-bool connect_bme(){
-  static bool connected = false;
-  if (!connected) {
-    connected = bme.begin(0x76);
-  }
-  if (!connected) {
-    snprintf (msg, 50, "Could not find BME280 sensor, check wiring!");
-    //Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("homesens/wohnzimmer/message", msg);
-  }
-  return connected;
-}
-
-void setup_wifi() {
-	delay(10);
-	Serial.println();
-	Serial.print("Connecting to ");
-	Serial.println(SSID);
-
+  // SETUP WIFI
+	Serial.println(String("Connecting to ") + SSID);
 	WiFi.begin(SSID, PSK);
 
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(500);
 		Serial.print(".");
 	}
+	Serial.println(" WiFi connected");
+	Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-	Serial.println("");
-	Serial.println("WiFi connected");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
+  // SETUP MQTT
+	client.setServer(MQTT_BROKER, PORT);
+  espClientNameString.toCharArray(espClientName, sizeof(espClientName));
+
+  // INIT BME SENSOR
+  static bool connected = false;
+  if (!connected) {
+    while(!bme.begin(0x76)){
+      Serial.println("Could not find BME280. Check Wiring!");
+      delay(500);
+    }
+  }
+  Serial.println("BME280 connected");
 }
 
 void reconnect() {
 	while (!client.connected()) {
-		Serial.print("Reconnecting...");
-		if (!client.connect("ESP8266Client_wohnzimmer")) {
+		Serial.println("Reconnecting...");
+		if (!client.connect(espClientName)) {
 			Serial.print("failed, rc=");
 			Serial.print(client.state());
 			Serial.println(" retrying in 5 seconds");
@@ -76,58 +71,75 @@ void reconnect() {
 		}
 	}
 }
+
 void loop() {
+  // CONNECTION CHECK
 	if (!client.connected()) {
     digitalWrite(LED_BUILTIN, LOW);
 		reconnect();
 	}
 
-  if (connect_bme()){
-    digitalWrite(LED_BUILTIN, LOW);
-    client.loop();
+  digitalWrite(LED_BUILTIN, LOW);
+  client.loop();
 
-    // SEND REAL STUFF
-    snprintf (msg, 50, "Alive since %ld milliseconds", millis());
-    //Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("homesens/wohnzimmer/message", msg);
+  // SEND REAL STUFF
+  char msg[50];
+  snprintf (msg, 50, "Alive since %ld milliseconds", millis());
+  Serial.println(msg);
+  String topicMsg_string = "homesens/" + mqtt_subtopic + "/msg";
+  char topicMsg [50];
+  topicMsg_string.toCharArray(topicMsg, sizeof(topicMsg));
+  client.publish(topicMsg, msg);
 
-    // SEND IP ADDRESS
-    char bufIp [20];
-    IPAddress ipaddr = WiFi.localIP();
-    sprintf(bufIp, "%d.%d.%d.%d", ipaddr[0], ipaddr[1], ipaddr[2], ipaddr[3]);
-    client.publish("homesens/wohnzimmer/ip", bufIp);
+  // SEND IP ADDRESS
+  char bufIp [20];
+  IPAddress ipaddr = WiFi.localIP();
+  sprintf(bufIp, "%d.%d.%d.%d", ipaddr[0], ipaddr[1], ipaddr[2], ipaddr[3]);
+  String topicIp_string = "homesens/" + mqtt_subtopic + "/ip";
+  char topicIp [50];
+  topicIp_string.toCharArray(topicIp, sizeof(topicIp));
+  client.publish(topicIp, bufIp);
 
-    // SEND MAC ADDRESS
-    String mac = String(WiFi.macAddress());
-    char bufMac [20];
-    mac.toCharArray(bufMac, 20);
-    client.publish("homesens/wohnzimmer/mac", bufMac);
+  // SEND MAC ADDRESS
+  String mac = String(WiFi.macAddress());
+  char bufMac [20];
+  mac.toCharArray(bufMac, 20);
+  String topicMac_string = "homesens/" + mqtt_subtopic + "/mac";
+  char topicMac [50];
+  topicMac_string.toCharArray(topicMac, sizeof(topicMac));
+  client.publish(topicMac, bufMac);
 
-    //GET VALUES
-    float temp = bme.readTemperature() + TEMP_SENS_OFFSET;
-    float pressure = bme.readPressure()/100;
-    float humidity = bme.readHumidity();
-    char buftemp[20];
-    char bufpressure[20];
-    char bufhumidity[20];
+  // SEND TEMPERATURE
+  float temp = bme.readTemperature() + TEMP_SENS_OFFSET;
+  char buftemp[20];
+  sprintf (buftemp, "%f", temp);
+  String topicTemperature_string = "homesens/" + mqtt_subtopic + "/temperature";
+  char topicTemperature [50];
+  topicTemperature_string.toCharArray(topicTemperature, sizeof(topicTemperature));
+  client.publish(topicTemperature, buftemp);
 
-    sprintf (buftemp, "%f", temp);
-    sprintf (bufpressure, "%f", pressure);
-    sprintf (bufhumidity, "%f", humidity);
+  // SEND PRESSURE
+  float pressure = bme.readPressure()/100;
+  char bufpressure[20];
+  sprintf (bufpressure, "%f", pressure);
+  String topicPressure_string = "homesens/" + mqtt_subtopic + "/pressure";
+  char topicPressure [50];
+  topicPressure_string.toCharArray(topicPressure, sizeof(topicPressure));
+  client.publish(topicPressure, buftemp);
 
-    //Serial.println(buftemp);
-    //Serial.println(bufpressure);
-    //Serial.println(bufhumidity);
-    client.publish("homesens/wohnzimmer/temperature", buftemp);
-    client.publish("homesens/wohnzimmer/pressure", bufpressure);
-    client.publish("homesens/wohnzimmer/humidity", bufhumidity);
+  // SEND HUMIDITY
+  float humidity = bme.readHumidity();
+  char bufhumidity[20];
+  sprintf (bufhumidity, "%f", humidity);
+  String topicHumidity_string = "homesens/" + mqtt_subtopic + "/humidity";
+  char topicHumidity [50];
+  topicHumidity_string.toCharArray(topicHumidity, sizeof(topicHumidity));
+  client.publish(topicHumidity, buftemp);
 
-    // FINISH
-    digitalWrite(LED_BUILTIN, HIGH);
+  // FINISH
+  digitalWrite(LED_BUILTIN, HIGH);
 
-  }
+  // WAIT FOR GIVEN TIME PERIOD
   delay(DELAY);
-  
 }
      
